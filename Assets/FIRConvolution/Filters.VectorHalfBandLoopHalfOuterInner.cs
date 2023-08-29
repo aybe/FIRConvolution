c69a0ecdb64371ffafe6c36a9df0ce0a9cb0243b
aybe <aybe@users.noreply.github.com>
2023-08-29 09:04:44 +0200

    Clean up half band half loop outer inner

--- Assets/FIRConvolution/Filters.VectorHalfBandLoopHalfOuterInner.cs
+++ Assets/FIRConvolution/Filters.VectorHalfBandLoopHalfOuterInner.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Numerics;
 using Unity.Mathematics;
 
 namespace FIRConvolution
@@ -13,46 +12,49 @@ namespace FIRConvolution
             var n = filter.HLength;
             var v = filter.VLength;
             var c = filter.HCenter;
+            var d = length - v;
+            var e = n - 1;
+            var f = v * 2;
 
-            var tEnd = n - 1; // TODO extract tEnd
-
-            for (var sample = 0; sample <= length - v; sample += v)
+            for (var sample = 0; sample <= d; sample += v)
             {
-                var zGet = Filter.UpdateZ(ref filter, source, sample, 4);
+                var pos = Filter.UpdateZ(ref filter, source, sample, v);
 
                 var sum = float4.zero;
 
-                int tap, len, idx;
+                var tap = filter.HOffset;
 
-                for (tap = filter.HOffset, len = c / (v * 2), idx = 0; idx < len; tap += v * 2, idx++)
+                var idx = 0;
+
+                int len;
+
+                for (len = c / f; idx < len; tap += f, idx++)
                 {
                     var h0 = h[tap + 0];
                     var h1 = h[tap + 2];
                     var h2 = h[tap + 4];
                     var h3 = h[tap + 6];
 
-                    // TODO these additions could be vectorized as well
-
                     // @formatter:off
-                    var i01A = zGet - (tap + 0); var i01B = zGet - (tEnd - (tap + 0));
-                    var i02A = zGet - (tap + 2); var i02B = zGet - (tEnd - (tap + 2));
-                    var i03A = zGet - (tap + 4); var i03B = zGet - (tEnd - (tap + 4));
-                    var i04A = zGet - (tap + 6); var i04B = zGet - (tEnd - (tap + 6));
+                    var i01A = pos - (tap + 0); var i01B = pos - (e - (tap + 0));
+                    var i02A = pos - (tap + 2); var i02B = pos - (e - (tap + 2));
+                    var i03A = pos - (tap + 4); var i03B = pos - (e - (tap + 4));
+                    var i04A = pos - (tap + 6); var i04B = pos - (e - (tap + 6));
 
-                    var i05A = zGet - (tap + 1); var i05B = zGet - (tEnd - (tap - 1));
-                    var i06A = zGet - (tap + 3); var i06B = zGet - (tEnd - (tap + 1));
-                    var i07A = zGet - (tap + 5); var i07B = zGet - (tEnd - (tap + 3));
-                    var i08A = zGet - (tap + 7); var i08B = zGet - (tEnd - (tap + 5));
+                    var i05A = pos - (tap + 1); var i05B = pos - (e - (tap - 1));
+                    var i06A = pos - (tap + 3); var i06B = pos - (e - (tap + 1));
+                    var i07A = pos - (tap + 5); var i07B = pos - (e - (tap + 3));
+                    var i08A = pos - (tap + 7); var i08B = pos - (e - (tap + 5));
 
-                    var i09A = zGet - (tap + 2); var i09B = zGet - (tEnd - (tap - 2));
-                    var i10A = zGet - (tap + 4); var i10B = zGet - (tEnd - (tap + 0));
-                    var i11A = zGet - (tap + 6); var i11B = zGet - (tEnd - (tap + 2));
-                    var i12A = zGet - (tap + 8); var i12B = zGet - (tEnd - (tap + 4));
+                    var i09A = pos - (tap + 2); var i09B = pos - (e - (tap - 2));
+                    var i10A = pos - (tap + 4); var i10B = pos - (e - (tap + 0));
+                    var i11A = pos - (tap + 6); var i11B = pos - (e - (tap + 2));
+                    var i12A = pos - (tap + 8); var i12B = pos - (e - (tap + 4));
 
-                    var i13A = zGet - (tap + 3); var i13B = zGet - (tEnd - (tap - 3));
-                    var i14A = zGet - (tap + 5); var i14B = zGet - (tEnd - (tap - 1));
-                    var i15A = zGet - (tap + 7); var i15B = zGet - (tEnd - (tap + 1));
-                    var i16A = zGet - (tap + 9); var i16B = zGet - (tEnd - (tap + 3));
+                    var i13A = pos - (tap + 3); var i13B = pos - (e - (tap - 3));
+                    var i14A = pos - (tap + 5); var i14B = pos - (e - (tap - 1));
+                    var i15A = pos - (tap + 7); var i15B = pos - (e - (tap + 1));
+                    var i16A = pos - (tap + 9); var i16B = pos - (e - (tap + 3));
 
                     var z01A = z[i01A]; var z01B = z[i01B];
                     var z02A = z[i02A]; var z02B = z[i02B];
@@ -96,27 +98,34 @@ namespace FIRConvolution
                         math.dot(hv0, zv6 + zv7));
                 }
 
-                // TODO process 4 floats 1 tap 1 tap hop symmetrical
-
-                for (; tap < c; tap += 2) // TODO DRY VectorHalfBandLoopHalfOuter
+                for (; tap < c; tap += 2)
                 {
                     var h0 = h[tap];
 
-                    // @formatter:off
-                    var i0 = zGet - tap - 0; var i4 = zGet - tEnd + tap - 0;
-                    var i1 = zGet - tap - 1; var i5 = zGet - tEnd + tap - 1;
-                    var i2 = zGet - tap - 2; var i6 = zGet - tEnd + tap - 2;
-                    var i3 = zGet - tap - 3; var i7 = zGet - tEnd + tap - 3;
+                    var i0 = pos - tap - 0;
+                    var i1 = pos - tap - 1;
+                    var i2 = pos - tap - 2;
+                    var i3 = pos - tap - 3;
 
-                    var z0 = z[i0]; var z4 = z[i4];
-                    var z1 = z[i1]; var z5 = z[i5];
-                    var z2 = z[i2]; var z6 = z[i6];
-                    var z3 = z[i3]; var z7 = z[i7];
-                    // @formatter:on
+                    var i4 = pos - e + tap - 0;
+                    var i5 = pos - e + tap - 1;
+                    var i6 = pos - e + tap - 2;
+                    var i7 = pos - e + tap - 3;
+
+                    var z0 = z[i0];
+                    var z1 = z[i1];
+                    var z2 = z[i2];
+                    var z3 = z[i3];
+
+                    var z4 = z[i4];
+                    var z5 = z[i5];
+                    var z6 = z[i6];
+                    var z7 = z[i7];
 
                     var hv0 = new float4(h0);
                     var zv1 = new float4(z0, z1, z2, z3);
                     var zv2 = new float4(z4, z5, z6, z7);
+
                     sum += hv0 * (zv1 + zv2);
                 }
 
