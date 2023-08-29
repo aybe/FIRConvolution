54d56ef218c14487622852cc96d0a76664a22e79
aybe <aybe@users.noreply.github.com>
2023-08-29 08:57:37 +0200

    Clean up half band half loop outer

--- Assets/FIRConvolution/Filters.VectorHalfBandLoopHalfOuter.cs
+++ Assets/FIRConvolution/Filters.VectorHalfBandLoopHalfOuter.cs
@@ -12,36 +12,41 @@ namespace FIRConvolution
             var n = filter.HLength;
             var v = filter.VLength;
             var c = filter.HCenter;
-
-            var tEnd = n - 1; // TODO extract tEnd
+            var e = n - 1;
 
             for (var sample = 0; sample < length; sample += v)
             {
-                var zGet = Filter.UpdateZ(ref filter, source, sample, 4);
+                var pos = Filter.UpdateZ(ref filter, source, sample, v);
 
                 var sum = float4.zero;
 
                 var tap = filter.HOffset;
 
-                // TODO process 4 floats 1 tap 1 tap hop symmetrical
-
                 for (; tap < c; tap += 2) // TODO DRY VectorHalfBandLoopHalfOuterInner
                 {
                     var h0 = h[tap];
 
-                    // @formatter:off
-                    var i0 = zGet - tap - 0; var i4 = zGet - tEnd + tap - 0;
-                    var i1 = zGet - tap - 1; var i5 = zGet - tEnd + tap - 1;
-                    var i2 = zGet - tap - 2; var i6 = zGet - tEnd + tap - 2;
-                    var i3 = zGet - tap - 3; var i7 = zGet - tEnd + tap - 3;
+                    var i0 = pos - tap - 0;
+                    var i1 = pos - tap - 1;
+                    var i2 = pos - tap - 2;
+                    var i3 = pos - tap - 3;
 
-                    var z0 = z[i0]; var z4 = z[i4];
-                    var z1 = z[i1]; var z5 = z[i5];
-                    var z2 = z[i2]; var z6 = z[i6];
-                    var z3 = z[i3]; var z7 = z[i7];
-                    // @formatter:on
+                    var i4 = pos - e + tap - 0;
+                    var i5 = pos - e + tap - 1;
+                    var i6 = pos - e + tap - 2;
+                    var i7 = pos - e + tap - 3;
 
-                    var hv0 = new float4(h0);
+                    var z0 = z[i0];
+                    var z1 = z[i1];
+                    var z2 = z[i2];
+                    var z3 = z[i3];
+
+                    var z4 = z[i4];
+                    var z5 = z[i5];
+                    var z6 = z[i6];
+                    var z7 = z[i7];
+
+                    var hv0 = new float4(h0, h0, h0, h0);
                     var zv0 = new float4(z0, z1, z2, z3);
                     var zv1 = new float4(z4, z5, z6, z7);
 
