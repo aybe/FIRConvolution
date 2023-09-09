21a3194eb15cb08acb43bde4e3a21b8666c0c215
aybe <aybe@users.noreply.github.com>
2023-09-09 07:40:53 +0200

    Move FilterType to library

---
+++ Assets/FIRConvolution/FilterType.cs
@@ -0,0 +1,18 @@
+namespace FIRConvolution
+{
+    public enum FilterType
+    {
+        ScalarFullBand,
+        ScalarHalfBandFullLoop,
+        ScalarHalfBandHalfLoop,
+        VectorFullBandInner,
+        VectorFullBandOuter,
+        VectorFullBandOuterInner,
+        VectorHalfBandFullLoopInner,
+        VectorHalfBandFullLoopOuter,
+        VectorHalfBandFullLoopOuterInner,
+        VectorHalfBandHalfLoopInner,
+        VectorHalfBandHalfLoopOuter,
+        VectorHalfBandHalfLoopOuterInner
+    }
+}
