737303f00d1bfe4b3a7f1706c659656bd42d4d35
aybe <aybe@users.noreply.github.com>
2023-09-11 20:06:58 +0200

    Add a help box in filter test editor

--- Assets/Scripts/FilterTestEditor.cs
+++ Assets/Scripts/FilterTestEditor.cs
@@ -5,5 +5,15 @@ using UnityEditor;
 internal sealed class FilterTestEditor : Editor
 {
     // BUG some more Unity shit -> no VU meter unless class has an editor!
+
+    public override void OnInspectorGUI()
+    {
+        if (EditorApplication.isPlaying)
+        {
+            EditorGUILayout.HelpBox("When profiling, collapse this component to avoid VU meter overhead.", MessageType.Warning, true);
+        }
+
+        base.OnInspectorGUI();
+    }
 }
 #endif
